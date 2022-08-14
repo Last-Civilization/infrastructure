bc379879e28e265d3aa167e0343acfe78154b3e3
mrrevo55 <kwolny31@gmail.com>
2022-08-14 19:32:52 +0200

    Add database configuration in vault_init.sh for user write service

--- vault_init.sh
+++ vault_init.sh
@@ -2,3 +2,4 @@ vault secrets disable secret
 vault secrets enable -path=secret kv
 vault write secret/application eureka.instance.hostname=localhost \ eureka.client.service-url.defaultZone=http://last-civilization:last-civilization@localhost:8761/eureka/
 vault write secret/discovery-service spring.security.user.name=last-civilization \ spring.security.user.password=last-civilization \ spring.security.user.roles=ADMIN
+vault write secret/user-write-service spring.datasource.url=jdbc:postgresql://localhost:5432/last-civilization_user_db \ spring.datasource.username=last-civilization \ spring.datasource.password=last-civilization
