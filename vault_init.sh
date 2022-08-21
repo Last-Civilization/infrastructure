7bac258ca3ddfc2e15dafce41f427ad32d97f6ec
mrrevo55 <kwolny31@gmail.com>
2022-08-21 21:59:27 +0200

    Start customize keycloak login page

--- vault_init.sh
+++ vault_init.sh
@@ -1,3 +1,4 @@
+sleep 5
 vault secrets disable secret
 vault secrets enable -path=secret kv
 vault write secret/application eureka.instance.hostname=localhost \ eureka.client.service-url.defaultZone=http://last-civilization:last-civilization@localhost:8761/eureka/
